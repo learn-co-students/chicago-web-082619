69cd82fce3e271f1ec2b522c56de30b2747c9858
Noa Heinrich <noawriteplay@gmail.com>
2019-09-11 11:55:35 -0500

    Update hero_spec.rb

--- new-tdd/spec/hero_spec.rb
+++ new-tdd/spec/hero_spec.rb
@@ -2,31 +2,32 @@ require_relative "../config/environment.rb"
 
 describe "Hero Class" do
 
+  before(:all) do
 
+    @iron_man = Hero.create(name: "Iron Man", ability: "Being rich", power_level: 50)
+  end
 
+  after(:all) do
+    @iron_man.destroy
+  end
   it "can train to increase power level" do
-    iron_man = Hero.create(name: "Iron Man", ability: "Being rich", power_level: 50)
-    iron_man.train(1)
-    expect(iron_man.power_level).to eq(52)
-    expect(iron_man.power_level).to be_a(Integer)
-    iron_man.destroy
+
+    @iron_man.train(1)
+    expect(@iron_man.power_level).to eq(52)
+    expect(@iron_man.power_level).to be_a(Integer)
   end
 
   it "ranks all heroes in order of power level" do
-    iron_man = Hero.create(name: "Iron Man", ability: "Being rich", power_level: 50)
     spiderman = Hero.create(name:"Spider-Man", ability:"Millennial", power_level: 25)
-    expect(Hero.ranked_heroes.first).to eq(iron_man)
+    expect(Hero.ranked_heroes.first).to eq(@iron_man)
     expect(Hero.ranked_heroes.last).to eq(spiderman)
     spiderman.destroy
-    iron_man.destroy
   end
 
   it "finds the strongest hero by power level" do
-    iron_man = Hero.create(name: "Iron Man", ability: "Being rich", power_level: 50)
     spiderman = Hero.create(name:"Spider-Man", ability:"Millennial", power_level: 25)
-    expect(Hero.find_strongest).to eq(iron_man)
+    expect(Hero.find_strongest).to eq(@iron_man)
     spiderman.destroy
-    iron_man.destroy
   end
 
   it "says its name" do
