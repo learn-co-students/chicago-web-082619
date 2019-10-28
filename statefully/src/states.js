d48934ea26a8aefd7e300adf6b502d32880f531c
dukegreene <dukegreene@gmail.com>
2019-10-28 16:13:04 -0500

    state events lecture prep

---
+++ statefully/src/states.js
@@ -0,0 +1,297 @@
+export default [
+    {
+        "name": "Alabama",
+        "abbreviation": "AL",
+        "flower": "Camellia"
+    },
+    {
+        "name": "Alaska",
+        "abbreviation": "AK",
+        "flower": "Alpine Forget-me-not"
+    },
+    {
+        "name": "American Samoa",
+        "abbreviation": "AS",
+        "flower": "Teuila"
+    },
+    {
+        "name": "Arizona",
+        "abbreviation": "AZ",
+        "flower": "Saguaro Cactus Blossom"
+    },
+    {
+        "name": "Arkansas",
+        "abbreviation": "AR",
+        "flower": "Apple Blossom"
+    },
+    {
+        "name": "California",
+        "abbreviation": "CA",
+        "flower": "California Poppy"
+    },
+    {
+        "name": "Colorado",
+        "abbreviation": "CO",
+        "flower": "Rocky Mountain Columbine"
+    },
+    {
+        "name": "Connecticut",
+        "abbreviation": "CT",
+        "flower": "Mountain Laurel"
+    },
+    {
+        "name": "Delaware",
+        "abbreviation": "DE",
+        "flower": "Peach Blossom"
+    },
+    {
+        "name": "District Of Columbia",
+        "abbreviation": "DC",
+        "flower": "American Beauty Rose"
+    },
+    {
+        "name": "Federated States Of Micronesia",
+        "abbreviation": "FM",
+        "flower": "Hibiscus"
+    },
+    {
+        "name": "Florida",
+        "abbreviation": "FL",
+        "flower": "Orange Blossom"
+    },
+    {
+        "name": "Georgia",
+        "abbreviation": "GA",
+        "flower": "Cherokee Rose"
+    },
+    {
+        "name": "Guam",
+        "abbreviation": "GU",
+        "flower": "Bougainvillea"
+    },
+    {
+        "name": "Hawaii",
+        "abbreviation": "HI",
+        "flower": "Hinahina"
+    },
+    {
+        "name": "Idaho",
+        "abbreviation": "ID",
+        "flower": "Syringa"
+    },
+    {
+        "name": "Illinois",
+        "abbreviation": "IL",
+        "flower": "Violet"
+    },
+    {
+        "name": "Indiana",
+        "abbreviation": "IN",
+        "flower": "Peony"
+    },
+    {
+        "name": "Iowa",
+        "abbreviation": "IA",
+        "flower": "Wild Rose"
+    },
+    {
+        "name": "Kansas",
+        "abbreviation": "KS",
+        "flower": "Wild Native Sunflower"
+    },
+    {
+        "name": "Kentucky",
+        "abbreviation": "KY",
+        "flower": "Goldenrod"
+    },
+    {
+        "name": "Louisiana",
+        "abbreviation": "LA",
+        "flower": "Magnolia"
+    },
+    {
+        "name": "Maine",
+        "abbreviation": "ME",
+        "flower": "White Pine Cone and Tassel"
+    },
+    {
+        "name": "Marshall Islands",
+        "abbreviation": "MH",
+        "flower": "Plumeria Frangipanis"
+    },
+    {
+        "name": "Maryland",
+        "abbreviation": "MD",
+        "flower": "Black-Eyed Susan"
+    },
+    {
+        "name": "Massachusetts",
+        "abbreviation": "MA",
+        "flower": "Mayflower"
+    },
+    {
+        "name": "Michigan",
+        "abbreviation": "MI",
+        "flower": "Apple Blossom"
+    },
+    {
+        "name": "Minnesota",
+        "abbreviation": "MN",
+        "flower": "Pink & White Lady Slipper"
+    },
+    {
+        "name": "Mississippi",
+        "abbreviation": "MS",
+        "flower": "Coreopsis"
+    },
+    {
+        "name": "Missouri",
+        "abbreviation": "MO",
+        "flower": "White Hawthorn Blossom"
+    },
+    {
+        "name": "Montana",
+        "abbreviation": "MT",
+        "flower": "Bitterroot"
+    },
+    {
+        "name": "Nebraska",
+        "abbreviation": "NE",
+        "flower": "Goldenrod"
+    },
+    {
+        "name": "Nevada",
+        "abbreviation": "NV",
+        "flower": "Sagebrush"
+    },
+    {
+        "name": "New Hampshire",
+        "abbreviation": "NH",
+        "flower": "Purple Lilac"
+    },
+    {
+        "name": "New Jersey",
+        "abbreviation": "NJ",
+        "flower": "Violet"
+    },
+    {
+        "name": "New Mexico",
+        "abbreviation": "NM",
+        "flower": "Yucca"
+    },
+    {
+        "name": "New York",
+        "abbreviation": "NY",
+        "flower": "Lilac"
+    },
+    {
+        "name": "North Carolina",
+        "abbreviation": "NC",
+        "flower": "Carolina Lily"
+    },
+    {
+        "name": "North Dakota",
+        "abbreviation": "ND",
+        "flower": "Wild Prairie Rose"
+    },
+    {
+        "name": "Northern Mariana Islands",
+        "abbreviation": "MP",
+        "flower": "Plumeria"
+    },
+    {
+        "name": "Ohio",
+        "abbreviation": "OH",
+        "flower": "Red Carnation"
+    },
+    {
+        "name": "Oklahoma",
+        "abbreviation": "OK",
+        "flower": "Mistletoe"
+    },
+    {
+        "name": "Oregon",
+        "abbreviation": "OR",
+        "flower": "Oregon Grape"
+    },
+    {
+        "name": "Palau",
+        "abbreviation": "PW",
+        "flower": "Rur"
+    },
+    {
+        "name": "Pennsylvania",
+        "abbreviation": "PA",
+        "flower": "Penngift Crownvetch"
+    },
+    {
+        "name": "Puerto Rico",
+        "abbreviation": "PR",
+        "flower": "Flor de Maga"
+    },
+    {
+        "name": "Rhode Island",
+        "abbreviation": "RI",
+        "flower": "Violet"
+    },
+    {
+        "name": "South Carolina",
+        "abbreviation": "SC",
+        "flower": "Goldenrod"
+    },
+    {
+        "name": "South Dakota",
+        "abbreviation": "SD",
+        "flower": "American Pasque"
+    },
+    {
+        "name": "Tennessee",
+        "abbreviation": "TN",
+        "flower": "Iris"
+    },
+    {
+        "name": "Texas",
+        "abbreviation": "TX",
+        "flower": "Ennis"
+    },
+    {
+        "name": "Utah",
+        "abbreviation": "UT",
+        "flower": "Sego Lily"
+    },
+    {
+        "name": "Vermont",
+        "abbreviation": "VT",
+        "flower": "Red Clover"
+    },
+    {
+        "name": "Virgin Islands",
+        "abbreviation": "VI",
+        "flower": "Yellow Cedar"
+    },
+    {
+        "name": "Virginia",
+        "abbreviation": "VA",
+        "flower": "American Dogwood"
+    },
+    {
+        "name": "Washington",
+        "abbreviation": "WA",
+        "flower": "Coast Rhododendron"
+    },
+    {
+        "name": "West Virginia",
+        "abbreviation": "WV",
+        "flower": "Rhododendron"
+    },
+    {
+        "name": "Wisconsin",
+        "abbreviation": "WI",
+        "flower": "Wood Violet"
+    },
+    {
+        "name": "Wyoming",
+        "abbreviation": "WY",
+        "flower": "Painted Cup"
+    }
+]
