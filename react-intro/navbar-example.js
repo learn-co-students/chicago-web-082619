f3c638c281d77c8e2bbc008b59fb1246c06882eb
dukegreene <dukegreene@gmail.com>
2019-10-28 11:22:46 -0500

    react intro lesson

--- react-intro/navbar-example.js
+++ react-intro/navbar-example.js
@@ -11,3 +11,28 @@
 //       </h2>
 //     </a>
 //   </div>
+
+// Let's try a thing! How could you make the following code return the above element?
+
+const Navbar = props => {
+  // ...
+  return React.createElement('div', { key: 'firstWrapperDiv01', className: `ui inverted ${props.color} menu`}, [
+    React.createElement('a', { className: 'item'}, [
+      React.createElement('h2', {key: 'firstH201'}, [
+        React.createElement('i', { className: `${props.icon} icon`, key: 'firstIcon01'}, []),
+        React.createElement('div', { className: 'content', key: 'firstDiv01'}, props.title),
+        React.createElement('div', { className: 'sub header', key: 'secondDiv01'}, props.description)
+      ])
+    ])
+  ])
+};
+
+ReactDOM.render(
+  Navbar({
+    title: 'ZooKeepr',
+    icon: 'paw',
+    color: 'orange',
+    description: 'keep track of your animals'
+  }),
+  document.getElementById('main')
+);
--- react-intro/src/index.js
+++ react-intro/src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// // index.html has the goods!
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
+// Under the CreateElement hood
 const myCreateElement = (type, props = {}, children) => {
   return {
     $$typeof: Symbol.for('react.element'),
@@ -11,13 +13,36 @@ const myCreateElement = (type, props = {}, children) => {
 };
 
 const Article = props => {
-  return myCreateElement('div', {}, [
-    myCreateElement('h1', {}, props.title),
-    myCreateElement('p', {}, props.text)
+  // <div>
+  //    <h1>TITLE</h1>
+  //    <p>TEXT</p>
+  // </div>
+
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.title),
+    React.createElement('p', {}, props.text)
   ]);
 };
 
+const ArticlesContainer = props => {
+  // if props contains articles e.g. props.articles
+  // map over them and create a collection of elements to render
+  const articleElements = props.articles.map(articleInfo => {
+    return Article(articleInfo);
+  })
+  return React.createElement('h3', props, articleElements)
+}
+
+// Make 3 articles
+const good = { title: 'Good News', text: 'probably something great...' }
+const bad = { title: 'Bad News', text: 'probably something bad...' }
+const middle = { title: 'Middle News', text: 'maybe nothing is real, just keep shopping...' }
+// put them in an array
+const articlesArray = [good, bad, middle]
+
+
+
 ReactDOM.render(
-  Article({ title: 'Some News', text: 'probably something bad...' }),
+  ArticlesContainer({ articles: articlesArray }),
   document.getElementById('main')
 );
